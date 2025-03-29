c50aebefb791ed1636573d2382de0f610357b543
Crodise <reg@shuyuzi.com>
2025-03-29 13:02:05 +0800

    Submit

---
+++ 对接coze/static/js/constants.js
@@ -0,0 +1,8 @@
+// Coze API 的请求地址
+const coze_api_url = 'https://api.coze.cn/v3/chat';
+// Coze API 的授权 Token
+const authorization_token = 'YOUR_TOKEN';	// 改成自己的
+// 智能体 ID
+const assistant_id = 'YOUR_ASSISTANT_ID';	// 改成自己的
+// 生成一个唯一的用户 ID
+const user_id = uuidv4();
