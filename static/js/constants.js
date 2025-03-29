5692e4347ddf2624217916e0ae99dbbe899178af
OvO <gyzuike@qq.com>
2025-03-29 16:48:07 +0800

    放弃coze

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,8 +1,8 @@
 // 腾讯元气 API 的请求地址
 const yuanqi_api_url = 'https://yuanqi.tencent.com/openapi/v1/agent/chat/completions';
 // 腾讯元气 API 的授权 Token
-const authorization_token = 'YOUR_TOKEN';	// 改成自己的
+const authorization_token = 'Bearer GeWlu4XhZrx1KXWU1W0GsG8oSe3pTOn4';	// 改成自己的
 // 智能体 ID
-const assistant_id = 'ASSISTANT_ID';	//改成自己的
+const assistant_id = 'QWGgrUK7ehrb';	//改成自己的
 // 生成一个唯一的用户 ID
 const user_id = uuidv4();
